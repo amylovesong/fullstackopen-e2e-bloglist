ef62d6b3e6e5246660a0c6497ee2066d08f1847c
amylovesong <amylovesong.sun@gmail.com>
2025-01-20 10:26:30 +0800

    Add part5 exercise-5.19

--- tests/bloglist_app.spec.js
+++ tests/bloglist_app.spec.js
@@ -43,4 +43,25 @@ describe('Blog app', () => {
       await expect(page.getByText('Amy Sun logged in')).not.toBeVisible()
     })
   })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByTestId('username').fill('amylovesong')
+      await page.getByTestId('password').fill('abcd')
+
+      await page.getByRole('button', { name: 'login' }).click()
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'new note' }).click()
+
+      await page.getByPlaceholder('write blog title here').fill('New blog created by test')
+      await page.getByPlaceholder('write blog author here').fill('Amy Sun')
+      await page.getByPlaceholder('write blog url here').fill('http://localhost:5173')
+    
+      await page.getByRole('button', { name: 'create' }).click()
+
+      await expect(page.getByText('New blog created by test Amy Sun')).toBeVisible()
+    })
+  })
 })
